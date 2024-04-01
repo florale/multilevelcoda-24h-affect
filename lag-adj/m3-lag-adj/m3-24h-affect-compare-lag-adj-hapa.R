cf4b5e2a2301baaf6d1aa2f6ac652bb2d5668d8f
Flora Le <flora.le@monash.edu>
2024-04-01 17:40:10 +1100

    seperate analyses by each affect type
    previously was too slow to break down to shorter analyses

---
+++ lag-adj/m3-lag-adj/m3-24h-affect-compare-lag-adj-hapa.R
@@ -0,0 +1,58 @@
+library(data.table)
+library(extraoperators)
+library(compositions)
+library(multilevelcoda)
+library(brms)
+library(cmdstanr)
+library(insight)
+
+library(doFuture)
+library(foreach)
+library(parallel)
+library(doRNG)
+library(future)
+library(loo)
+
+## models---------
+m_hapa_lag_adj <- readRDS("m_hapa_lag_adj.RDS")
+m_lapa_lag_adj <- readRDS("m_lapa_lag_adj.RDS")
+m_hana_lag_adj <- readRDS("m_hana_lag_adj.RDS")
+m_lana_lag_adj <- readRDS("m_lana_lag_adj.RDS")
+
+mb_hapa_lag_adj <- readRDS("mb_hapa_lag_adj.RDS")
+mb_lapa_lag_adj <- readRDS("mb_lapa_lag_adj.RDS")
+mb_hana_lag_adj <- readRDS("mb_hana_lag_adj.RDS")
+mb_lana_lag_adj <- readRDS("mb_lana_lag_adj.RDS")
+
+mw_hapa_lag_adj <- readRDS("mw_hapa_lag_adj.RDS")
+mw_lapa_lag_adj <- readRDS("mw_lapa_lag_adj.RDS")
+mw_hana_lag_adj <- readRDS("mw_hana_lag_adj.RDS")
+mw_lana_lag_adj <- readRDS("mw_lana_lag_adj.RDS")
+
+m0_hapa_lag_adj <- readRDS("m0_hapa_lag_adj.RDS")
+m0_lapa_lag_adj <- readRDS("m0_lapa_lag_adj.RDS")
+m0_hana_lag_adj <- readRDS("m0_hana_lag_adj.RDS")
+m0_lana_lag_adj <- readRDS("m0_lana_lag_adj.RDS")
+
+## loo ------------
+options(mc.cores = 20)
+
+loo_m_hapa_lag_adj <- loo(m_hapa_lag_adj$Model, reloo = TRUE, 
+                          reloo_args = list(recompile = TRUE, 
+                                            future_args = list(globals = list(future.globals.maxSize = 850*1024^2))))
+saveRDS(loo_m_hapa_lag_adj, "loo_m_hapa_lag_adj.RDS")
+
+loo_mb_hapa_lag_adj <- loo(mb_hapa_lag_adj$Model, reloo = TRUE, 
+                           reloo_args = list(recompile = TRUE, 
+                                             future_args = list(globals = list(future.globals.maxSize = 850*1024^2))))
+saveRDS(loo_mb_hapa_lag_adj, "loo_mb_hapa_lag_adj.RDS")
+
+loo_mw_hapa_lag_adj <- loo(mw_hapa_lag_adj$Model, reloo = TRUE, 
+                           reloo_args = list(recompile = TRUE, 
+                                             future_args = list(globals = list(future.globals.maxSize = 850*1024^2))))
+saveRDS(loo_mw_hapa_lag_adj, "loo_mw_hapa_lag_adj.RDS")
+
+loo_m0_hapa_lag_adj <- loo(m0_hapa_lag_adj$Model, reloo = TRUE, 
+                           reloo_args = list(recompile = TRUE, 
+                                             future_args = list(globals = list(future.globals.maxSize = 850*1024^2))))
+saveRDS(loo_m0_hapa_lag_adj, "loo_m0_hapa_lag_adj.RDS")
---
+++ lag-adj/m3-lag-adj/m3-24h-affect-compare-lag-adj-lana.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=m3-24h-affect-compare-lag-adj-lana
+#SBATCH --time=2-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=5GB
+#SBATCH --cpus-per-task=20
+#SBATCH --partition=comp
+
+cd /fs04/ft29/multilevelcoda-24h-affect
+
+module load R/4.2.2-mkl
+
+Rscript m3-24h-affect-compare-lag-adj-lana.R
+
+echo 'Completed by' $USER

5c4fa9d2eb51688fb28d0be21e1d019ecd6d4a6e
Flora Le <flora.le@monash.edu>
2024-04-04 01:32:28 +1100

    rename for consistency with other analyses

--- 24h-affect-sub.R
+++ 24h-affect-sub.R
@@ -1,20 +1,20 @@
 source("24h-affect-utils.R")
 
-mhapa_ww_sub <- readRDS(paste0(outputdir, "mhapa_ww_sub", ".RDS"))
-mlapa_ww_sub <- readRDS(paste0(outputdir, "mlapa_ww_sub", ".RDS"))
-mhana_ww_sub <- readRDS(paste0(outputdir, "mhana_ww_sub", ".RDS"))
-mlana_ww_sub <- readRDS(paste0(outputdir, "mlana_ww_sub", ".RDS"))
+m_hapa_sub <- readRDS(paste0(outputdir, "m_hapa_sub", ".RDS"))
+m_lapa_sub <- readRDS(paste0(outputdir, "m_lapa_sub", ".RDS"))
+m_hana_sub <- readRDS(paste0(outputdir, "m_hana_sub", ".RDS"))
+m_lana_sub <- readRDS(paste0(outputdir, "m_lana_sub", ".RDS"))
 
 ## substitution summary 
-summary(mhapa_ww_sub, delta = 30)
-summary(mlapa_ww_sub, delta = 30)
-summary(mhana_ww_sub, delta = 30)
-summary(mlana_ww_sub, delta = 30)
+summary(m_hapa_sub, delta = 30)
+summary(m_lapa_sub, delta = 30)
+summary(m_hana_sub, delta = 30)
+summary(m_lana_sub, delta = 30)
 
-summary(mhapa_ww_sub, delta = 60, digits = 3)
-summary(mlapa_ww_sub, delta = 60, digits = 3)
-summary(mhana_ww_sub, delta = 60, digits = 3)
-summary(mlana_ww_sub, delta = 60, digits = 3)
+summary(m_hapa_sub, delta = 60, digits = 3)
+summary(m_lapa_sub, delta = 60, digits = 3)
+summary(m_hana_sub, delta = 60, digits = 3)
+summary(m_lana_sub, delta = 60, digits = 3)
 
 summary(mhapa_ww_lag_sub_adj, delta = 1, digits = 5)
 summary(mlapa_ww_lag_sub_adj, delta = 1, digits = 5)

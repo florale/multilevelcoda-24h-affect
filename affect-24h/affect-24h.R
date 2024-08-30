9021af1c30640f31f2a657b241c63cadbf0dd60f
Flora Le <flora.le@monash.edu>
2024-08-30 10:48:31 +1000

    clean up

--- affect-24h/affect-24h.R
+++
@@ -1,8 +0,0 @@
-source("24h-affect-utils.R")
-
-# models -----------------
-m_hapa_24h <- readRDS(paste0(outputdir, "m_hapa_24h", ".RDS"))
-m_lapa_24h <- readRDS(paste0(outputdir, "m_lapa_24h", ".RDS"))
-m_hana_24h <- readRDS(paste0(outputdir, "m_hana_24h", ".RDS"))
-m_lana_24h <- readRDS(paste0(outputdir, "m_lana_24h", ".RDS"))
-

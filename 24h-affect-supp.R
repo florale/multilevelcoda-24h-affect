4f0e7cdc46abac0ce37c690d2889ade2c44c6be5
Flora Le <flora.le@monash.edu>
2025-07-31 10:07:37 +1000

    update supp file

--- 24h-affect-supp.R
+++ 24h-affect-supp.R
@@ -1,12 +1,23 @@
-source("24h-affect-utils.R")
+source("24h-affect-setup.R")
 
-# # brmcoda
-# m_hapa <- readRDS(paste0(outputdir, "m_hapa", ".RDS"))
-# m_lapa <- readRDS(paste0(outputdir, "m_lapa", ".RDS"))
-# m_hana <- readRDS(paste0(outputdir, "m_hana", ".RDS"))
-# m_lana <- readRDS(paste0(outputdir, "m_lana", ".RDS"))
+# # brmcoda ------------------------------------------------
+m_hapa <- readRDS(paste0(outputdir, "m_hapa", ".RDS"))
+m_lapa <- readRDS(paste0(outputdir, "m_lapa", ".RDS"))
+m_hana <- readRDS(paste0(outputdir, "m_hana", ".RDS"))
+m_lana <- readRDS(paste0(outputdir, "m_lana", ".RDS"))
 
-# substitution
+# rhat and ess, mcmc diagnostics
+rhat_summary <- readRDS(paste0(outputdir, "rhat_summary.RDS"))
+ess_bulk_summary <- readRDS(paste0(outputdir, "ess_bulk_summary.RDS"))
+ess_tail_summary <- readRDS(paste0(outputdir, "ess_tail_summary.RDS"))
+
+plot_mcmc_hapa <- readRDS(paste0(outputdir, "plot_mcmc_hapa.RDS"))
+plot_mcmc_lapa <- readRDS(paste0(outputdir, "plot_mcmc_lapa.RDS"))
+plot_mcmc_hana <- readRDS(paste0(outputdir, "plot_mcmc_hana.RDS"))
+plot_mcmc_lana <- readRDS(paste0(outputdir, "plot_mcmc_lana.RDS"))
+
+# substitution ------------------------------------------------
+## adjusted models
 m_hapa_sub <- readRDS(paste0(outputdir, "m_hapa_sub", ".RDS"))
 m_lapa_sub <- readRDS(paste0(outputdir, "m_lapa_sub", ".RDS"))
 m_hana_sub <- readRDS(paste0(outputdir, "m_hana_sub", ".RDS"))
@@ -16,7 +27,8 @@ h24_affect_sub_list <- lapply(list(
   m_hapa_sub, m_lapa_sub, m_hana_sub, m_lana_sub
 ), function(X) {
   
-  X <- summary(X, delta = 30)
+  X <- summary(X, delta = 30, digits = "asis")
+  
   X[, From := ifelse(From == "Sleepg", "Sleep", From)]
   X[, From := ifelse(From == "WAKEg", "Awake in Bed", From)]
   X[, From := ifelse(From == "MVPAg", "MVPA", From)]
@@ -54,3 +66,75 @@ names <- c(`Sleepg` = "Sleep",
 labeller <- function(variable,value){
   return(names[value])
 }
+
+## unadjusted models
+m_hapa_unadj_sub <- readRDS(paste0(outputdir, "m_hapa_unadj_sub", ".RDS"))
+m_lapa_unadj_sub <- readRDS(paste0(outputdir, "m_lapa_unadj_sub", ".RDS"))
+m_hana_unadj_sub <- readRDS(paste0(outputdir, "m_hana_unadj_sub", ".RDS"))
+m_lana_unadj_sub <- readRDS(paste0(outputdir, "m_lana_unadj_sub", ".RDS"))
+
+h24_affect_unadj_sub_list <- lapply(list(
+  m_hapa_unadj_sub, m_lapa_unadj_sub, m_hana_unadj_sub, m_lana_unadj_sub
+), function(X) {
+
+  X <- summary(X, delta = 30, digits = "asis")
+
+  X[, From := ifelse(From == "Sleepg", "Sleep", From)]
+  X[, From := ifelse(From == "WAKEg", "Awake in Bed", From)]
+  X[, From := ifelse(From == "MVPAg", "MVPA", From)]
+  X[, From := ifelse(From == "LPAg", "LPA", From)]
+  X[, From := ifelse(From == "SBg", "SB", From)]
+
+  X[, To := ifelse(To == "Sleepg", "Sleep", To)]
+  X[, To := ifelse(To == "WAKEg", "Awake in Bed", To)]
+  X[, To := ifelse(To == "MVPAg", "MVPA", To)]
+  X[, To := ifelse(To == "LPAg", "LPA", To)]
+  X[, To := ifelse(To == "SBg", "SB", To)]
+
+  X$Sig <- between(0, X$CI_low, X$CI_high)
+  X[, Sig := ifelse(Sig == FALSE & Delta %in% c(-30, 30), "Yes", "")]
+
+  X[, `Estimate [95% CI]` := paste0(format(round(Mean, digits = 2), nsmall = 2),
+                                    " [",
+                                    format(round(CI_low, digits = 2), nsmall = 2),
+                                    ", ",
+                                    format(round(CI_high, digits = 2), nsmall = 2),
+                                    "]")]
+  X <- X[, .(`Estimate [95% CI]`, Minute = Delta, From, To, Level, Sig)]
+})
+
+## sen models
+m_hapa_sen_sub <- readRDS(paste0(outputdir, "m_hapa_sen_sub", ".RDS"))
+m_lapa_sen_sub <- readRDS(paste0(outputdir, "m_lapa_sen_sub", ".RDS"))
+m_hana_sen_sub <- readRDS(paste0(outputdir, "m_hana_sen_sub", ".RDS"))
+m_lana_sen_sub <- readRDS(paste0(outputdir, "m_lana_sen_sub", ".RDS"))
+
+h24_affect_sen_sub_list <- lapply(list(
+  m_hapa_sen_sub, m_lapa_sen_sub, m_hana_sen_sub, m_lana_sen_sub
+), function(X) {
+  
+  X <- summary(X, delta = 30, digits = "asis")
+  
+  X[, From := ifelse(From == "Sleepg", "Sleep", From)]
+  X[, From := ifelse(From == "WAKEg", "Awake in Bed", From)]
+  X[, From := ifelse(From == "MVPAg", "MVPA", From)]
+  X[, From := ifelse(From == "LPAg", "LPA", From)]
+  X[, From := ifelse(From == "SBg", "SB", From)]
+  
+  X[, To := ifelse(To == "Sleepg", "Sleep", To)]
+  X[, To := ifelse(To == "WAKEg", "Awake in Bed", To)]
+  X[, To := ifelse(To == "MVPAg", "MVPA", To)]
+  X[, To := ifelse(To == "LPAg", "LPA", To)]
+  X[, To := ifelse(To == "SBg", "SB", To)]
+  
+  X$Sig <- between(0, X$CI_low, X$CI_high)
+  X[, Sig := ifelse(Sig == FALSE & Delta %in% c(-30, 30), "Yes", "")]
+  
+  X[, `Estimate [95% CI]` := paste0(format(round(Mean, digits = 2), nsmall = 2),
+                                    " [",
+                                    format(round(CI_low, digits = 2), nsmall = 2),
+                                    ", ",
+                                    format(round(CI_high, digits = 2), nsmall = 2),
+                                    "]")]
+  X <- X[, .(`Estimate [95% CI]`, Minute = Delta, From, To, Level, Sig)]
+})
